d62bfe63e74b2052258c942641c1334d83d558ea
Mikail Gubanov <yainformal@ya.ru>
2023-04-28 01:03:25 +0500

    в models.py реализована функция хранения пароля в БД в виде ХЭШа

--- LinguaLearn/views.py
+++ LinguaLearn/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
 
 
 def index(request):
@@ -38,10 +39,11 @@ def customer_registered(request):
             format_birth_date = datetime.datetime.strptime(birth_date, '%d.%m.%Y').date()
             email = request.POST.get('email')
             password = request.POST.get('password')
+            hashed_password = make_password(password)
 
             customer = CustomUser(
                 email=email,
-                password=password,
+                password=hashed_password,
                 first_name=first_name,
                 last_name=last_name,
                 birth_date=format_birth_date
