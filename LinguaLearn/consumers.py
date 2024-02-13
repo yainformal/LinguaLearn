7f1763d7969bf33510dfe36ad1ce9fdd9d0ff56a
Mikail Gubanov <yainformal@ya.ru>
2024-02-13 12:45:25 +0300

    Добавлено логирование в режиме INFO

--- LinguaLearn/consumers.py
+++ LinguaLearn/consumers.py
@@ -1,23 +1,30 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import logging
+
+# Получение инстанса логгера
+logger = logging.getLogger('django.channels')
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        logger.info('WebSocket соединение установлено')
 
     async def disconnect(self, close_code):
-        pass
+        logger.info(f'WebSocket соединение закрыто: {close_code}')
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         sender = text_data_json['sender']
+        logger.info(f'Получено сообщение от {sender}: {message}')
 
         if sender == 'user':
             await self.send_bot_message(message)
 
     async def send_bot_message(self, message):
         response = "Ответ от бота на ваше сообщение: " + message
+        logger.info(f'Отправка ответа от бота: {response}')
         await self.send(text_data=json.dumps({
             'message': response,
             'sender': 'bot'
--- LinguaLearn/settings.py
+++ LinguaLearn/settings.py
@@ -130,3 +130,19 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.channels': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
